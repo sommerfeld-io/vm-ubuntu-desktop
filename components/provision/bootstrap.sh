0bd8160d4684195216ee7e2b0ef315136dcc3a8b
Sebastian <sebastian@sommerfeld.io>
2024-10-22 09:08:38 +0000

    refactor: docker compose files should not be executable

--- components/provision/bootstrap.sh
+++ components/provision/bootstrap.sh
@@ -138,8 +138,9 @@ echo "[INFO] Download Docker Compose config for Portainer from repository"
 sudo curl -fsSL -o "$base_dir/portainer/docker-compose.yml" https://raw.githubusercontent.com/sommerfeld-io/vm-ubuntu/refs/heads/main/components/portainer/docker-compose.yml
 
 echo "[INFO] Set permissions in $base_dir"
-sudo chmod -R 755 "$base_dir"
 sudo chown -R "$(id -u):$(id -g)" "$base_dir"
+sudo chmod -R 755 "$base_dir"
+sudo chmod -x "$base_dir" "$base_dir/portainer/docker-compose.yml"
 
 echo "[INFO] ========================================================"
 echo "[INFO] Maybe you still need to add the user to the docker group"
--- components/test-compliance/vm-ubuntu/controls/opt.rb
+++ components/test-compliance/vm-ubuntu/controls/opt.rb
@@ -2,8 +2,8 @@ title "/opt/vm-ubuntu checks"
 
 control "opt-01" do
     impact 0.7
-    title "Files should exist and should have correct permissions"
-    desc "Check if files exist and have the correct permissions."
+    title "Scripts should exist and should have correct permissions"
+    desc "Check if scripts exist and have the correct permissions."
 
     FILES = %w(
         '/opt/vm-ubuntu/minikube-startup.sh'
@@ -20,6 +20,28 @@ control "opt-01" do
             it { should_not be_writable.by('group') }
             it { should_not be_writable.by('others') }
             it { should be_executable }
+        end
+    end
+end
+
+control "opt-02" do
+    impact 0.7
+    title "Docker compose files should exist and should have correct permissions"
+    desc "Check if docker compose files exist and have the correct permissions."
+
+    FILES = %w(
+        '/opt/vm-ubuntu/portainer/docker-compose.yml'
+    )
+    FILES.each do |folder|
+        describe file(folder) do
+            it { should exist }
+            it { should be_file }
+            it { should_not be_directory }
+            it { should be_readable }
+            it { should be_writable.by('owner') }
+            it { should_not be_writable.by('group') }
+            it { should_not be_writable.by('others') }
+            it { should_not be_executable }
         end
     end
 end
